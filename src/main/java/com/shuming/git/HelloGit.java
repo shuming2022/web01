2986289d39384f3890f6671422362c9692c29228
shuming2022 <jybwsm@gmail.com>
2022-07-18 17:46:17 +0800

    分支添加

--- src/main/java/com/shuming/git/HelloGit.java
+++ src/main/java/com/shuming/git/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 	System.out.println("Hello Git!");
 	System.out.println("更新1");
-	//System.out.println("更新2");
+	System.out.println("分支添加");
 	System.out.println(" 创建分支");
 	}
 
